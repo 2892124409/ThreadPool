d1b4c9b5b3a0643cefe51a84fd0658a6636afc10
taosir_hyt <2892124409@qq.com>
2026-01-19 15:51:19 +0800

    重构线程池类，使用环形缓冲区替代任务队列，并优化构造函数和任务提交逻辑

--- ThreadPool.h
+++ ThreadPool.h
@@ -10,12 +10,13 @@
 #include <future>
 #include <functional>
 #include <stdexcept>
+#include "RingBuffer.h"
 
 class ThreadPool
 {
 public:
     // 构造函数：创建并启动指定数量的线程
-    ThreadPool(size_t threads);
+    ThreadPool(size_t threads, size_t capacity = 100);
 
     // 析构函数：停止并销毁线程池
     ~ThreadPool();
@@ -32,8 +33,8 @@ private:
     // 工作线程的容器
     std::vector<std::thread> m_workers;
 
-    // 任务队列
-    std::queue<std::function<void()>> m_tasks;
+    // 环形任务队列
+    RingBuffer<std::function<void()>> m_tasks;
 
     // 同步原语
     std::mutex m_queue_mutex;
@@ -42,7 +43,7 @@ private:
 };
 
 // 构造函数实现
-inline ThreadPool::ThreadPool(size_t threads) : m_stop(false)
+inline ThreadPool::ThreadPool(size_t threads, size_t capacity) : m_stop(false), m_tasks(capacity)
 {
     for (size_t i = 0; i < threads; ++i)
     {
@@ -59,17 +60,20 @@ inline void ThreadPool::worker_loop()
         std::function<void()> task;
         {
             std::unique_lock<std::mutex> lock(this->m_queue_mutex);
+            // 当线程池停止或者任务队列非空就继续执行，否则该工作线程会释放锁并阻塞等待
             this->m_condition.wait(lock, [this]
-                                   { return this->m_stop || !this->m_tasks.empty(); });
-
-            if (this->m_stop && this->m_tasks.empty())
+                                   { return this->m_stop || !this->m_tasks.isEmpty(); });
+            // 判断如果线程池停止并且任务队列为空就直接返回，此时自动释放锁
+            if (this->m_stop && this->m_tasks.isEmpty())
             {
                 return;
             }
-
-            task = std::move(this->m_tasks.front());
-            this->m_tasks.pop();
+            // 从任务队列取任务
+            this->m_tasks.pop(task);
         }
+        // 通知生产者（可能在取任务前，队列是满的，生产者正在沉睡）
+        this->m_condition.notify_all();
+        // 执行任务
         task();
     }
 }
@@ -88,13 +92,18 @@ auto ThreadPool::submit(F &&f, Args &&...args)
     std::future<return_type> res = task->get_future();
     {
         std::unique_lock<std::mutex> lock(m_queue_mutex);
+        // 当线程池停止或任务队列非满就持有锁继续执行；如果线程池没有停止且任务队列满了当前线程就会释放锁并进入阻塞
+        m_condition.wait(lock, [this]
+                         { return m_stop || !m_tasks.isFull(); });
         if (m_stop)
         {
             throw std::runtime_error("submit on stopped ThreadPool");
         }
-        m_tasks.emplace([task]()
-                        { (*task)(); });
+
+        m_tasks.push([task]()
+                     { (*task)(); });
     }
+    // 通知消费者
     m_condition.notify_one();
     return res;
 }
